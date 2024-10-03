62ef083b8f72d527bd3baef2a2a87cf2d735856e
B3Y0NDER <dine.terzi@epitech.digital>
2024-10-03 13:23:37 +0200

    Create exo_03.js

---
+++ exo_03.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function () {             
+    document.getElementById("description").innerHTML = "You’re able to do more with your iPhone<br> than ever before.<br/><br>So iOS 22 reimagines the most<br> iconic parts of the experience to be<br> even more helpful and personal.<br><br/>Get yours now !<br/><br>Now featuring a headphone jack!"
+ });
