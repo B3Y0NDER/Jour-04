4828d49998a19f7ef00b828bf7aba570048cd9f4
B3Y0NDER <dine.terzi@epitech.digital>
2024-10-03 17:03:47 +0200

    Create exo_06.js

---
+++ exo_06.js
@@ -0,0 +1,6 @@
+window.addEventListener('resize', function() {
+alert('Size matters');
+})
+document.querySelector('img').addEventListener('mouseenter', function() {
+alert("Pretty, isn't it?");
+});
